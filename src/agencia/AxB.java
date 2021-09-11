c7862588f56cfe26b502a01b6db5304cc9bdc47b
CTMagnus <braianboianelli3@gmail.com>
2021-09-11 16:18:39 -0300

    TIEMPO ARREGLADO, ARCHIVOS DE SALIDA LISTOS

---
+++ src/agencia/AxB.java
@@ -0,0 +1,64 @@
+package agencia;
+import java.util.*;
+
+
+
+public class AxB extends Promocion {
+
+	List<Producto> atraccionesContenidas = new ArrayList<Producto>();
+	
+	protected boolean atraccionConCupo = true;
+	private double costo = 0;
+	private double tiempo;
+	private double descuento = 0;
+	
+	public AxB(TipoDeDescuento tipoDescuento,  tipoDeProducto tipo, 
+			tipoDeAtraccion tipoAtraccion,String nombre,double costo, List<Atraccion> lista) {
+		super(tipo, tipoAtraccion,nombre);
+		atraccionesContenidas.addAll(lista);
+		this.setTiempo();
+		
+	}
+	
+	
+	@Override
+	public double getPrecio() {
+		
+		for (int i = 0; i < atraccionesContenidas.size()-1; i++) {
+			costo += atraccionesContenidas.get(i).getPrecio();
+		}
+		return this.costo;
+	}
+	
+	public void setTiempo() {
+		
+		for (int i = 0; i < atraccionesContenidas.size(); i++) {
+			this.tiempo += atraccionesContenidas.get(i).getTiempo();
+		}
+		
+	}
+	
+	public double getTiempo() {
+		return this.tiempo;
+	}
+	
+	public void agregarAtraccionesContenidas(List<Atraccion> lista) {
+		atraccionesContenidas.addAll(lista);
+	}
+	
+	public void reducirCupoPromocion(agencia a1) {
+		for (int i = 0; i < atraccionesContenidas.size(); i++) {
+			int posicionDeLaAtraccion = a1.listaDeAtracciones.indexOf(atraccionesContenidas.get(i));
+			a1.listaDeAtracciones.get(posicionDeLaAtraccion).reducirCupo();
+			if(a1.listaDeAtracciones.get(posicionDeLaAtraccion).getAtraccionConCupo()!=true) this.atraccionConCupo = false;
+		}
+	}
+	
+	@Override
+	public String toString() {
+		String retorno = this.getNombre() + " " + this.getPrecio() + " " + atraccionesContenidas;
+		return retorno;
+	}
+
+	
+}
